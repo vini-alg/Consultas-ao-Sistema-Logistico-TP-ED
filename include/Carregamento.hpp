7aeacd6c01430070de2543086cf84469c9a4f8a0
vinicius <vinicius.alcantara115@hotmail.com>
2025-07-02 11:25:20 -0300

    UPDATE: carregamento de arquivos

--- include/Carregamento.hpp
+++ include/Carregamento.hpp
@@ -3,13 +3,26 @@
 
 #include <iostream>
 #include <string>
+#include "Evento.hpp"
+#include "Lista.hpp"
+#include "Mapa.hpp"
 
 class Carregamento {
-public:
-    Carregamento(const std::string& filename);
-    void executar();
 private:
     std::string filename;
+    Lista<Evento*> eventos; // Continua sendo a fonte da verdade para todos os eventos
+
+    // Índices para acesso rápido
+    Mapa<int, Lista<Evento*>*>* mapaPacotes;
+    Mapa<std::string, Lista<int>*>* mapaClientes;
+
+    void processarLinha(const std::string& linha);
+    TipoEvento stringParaTipoEvento(const std::string& s);
+
+public:
+    Carregamento(const std::string& filename);
+    ~Carregamento();
+    void executar();
 };
 
 #endif // CARREGAMENTO_HPP
